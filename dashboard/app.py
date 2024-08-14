8f11ac308b8cb4867f10e0d88ea42a84e424cfd4
ipy06 <Ipinoluwaheze@gmail.com>
2024-08-13 22:45:28 -0500

    changed the time range for the plots to show the latest data

--- dashboard/app.py
+++ dashboard/app.py
@@ -12,42 +12,25 @@ from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
-time = datetime.strptime("2024-07-30 19:37:20", "%Y-%m-%d %H:%M:%S")
-# Select time for date and time for cpu and memory usage plots
-t1c_m = "2024-07-30 19:37:20"
-t2c_m = (time + timedelta(minutes=60)).strftime("%Y-%m-%d %H:%M:%S")
-
-# Select time for date and time for DISK and Network data
-t1d_n = "2024-07-30 19:37:20"
-t2d_n = (time + timedelta(minutes=90)).strftime("%Y-%m-%d %H:%M:%S")
-
-# # After setting Up Cron tab
-# # Set t1 to 30 minutes before now and t2 to now
-# t2 = datetime.now()
-# t1 = t2 - timedelta(minutes=30)
-# t1_disk = t2 - timedelta(days=2)
+# After setting Up Cron tab
+t2 = datetime.now()
+t1 = t2 - timedelta(days=1)
+t1_disk = t2 - timedelta(days=10)
 
 
-# # Convert to string format expected by your plot functions
-# t1_str = t1.strftime('%Y-%m-%d %H:%M:%S')
-# t2_str = t2.strftime('%Y-%m-%d %H:%M:%S')
-# t1_disk_str = t1_disk.strftime('%Y-%m-%d %H:%M:%S')
+# Convert to string format expected by your plot functions
+t1_str = t1.strftime('%Y-%m-%d %H:%M:%S')
+t2_str = t2.strftime('%Y-%m-%d %H:%M:%S')
+t1_disk_str = t1_disk.strftime('%Y-%m-%d %H:%M:%S')
 
 @app.route('/')
 def index():
     # Generate the Plotly plots
-    cpu_plot = plotlyCPU.plotData(plotlyCPU.grabData(t1c_m, t2c_m)).to_html(full_html=False)
-    memory_plot = plotlyRAM.plotData(plotlyRAM.grabData(t1c_m, t2c_m)).to_html(full_html=False)
-    network_plot = plotlyNETWORK.plotData(plotlyNETWORK.grabData(t1d_n, t2d_n)).to_html(full_html=False)
-    disk_plot = plotlyDISK.plotData(plotlyDISK.grabData(t1d_n, t2d_n)).to_html(full_html=False)
-    combined_plot = combinedPlot.plot(t1c_m, t2d_n, t1d_n).to_html(full_html=False)
-
-    # # Generate the Plotly plots
-    # cpu_plot = plotlyCPU.plotData(plotlyCPU.grabData(t1_str, t2_str)).to_html(full_html=False)
-    # memory_plot = plotlyRAM.plotData(plotlyRAM.grabData(t1_str, t2_str)).to_html(full_html=False)
-    # network_plot = plotlyNETWORK.plotData(plotlyNETWORK.grabData(t1_str, t2_str)).to_html(full_html=False)
-    # disk_plot = plotlyDISK.plotData(plotlyDISK.grabData(t1_disk_str, t2_str)).to_html(full_html=False)
-    # combined_plot = combinedPlot.plot(t1d_n, t2d_n, t1_disk_str).to_html(full_html=False)
+    cpu_plot = plotlyCPU.plotData(plotlyCPU.grabData(t1_str, t2_str)).to_html(full_html=False)
+    memory_plot = plotlyRAM.plotData(plotlyRAM.grabData(t1_str, t2_str)).to_html(full_html=False)
+    network_plot = plotlyNETWORK.plotData(plotlyNETWORK.grabData(t1_str, t2_str)).to_html(full_html=False)
+    disk_plot = plotlyDISK.plotData(plotlyDISK.grabData(t1_disk_str, t2_str)).to_html(full_html=False)
+    combined_plot = combinedPlot.plot(t1_str, t2_str, t1_disk_str).to_html(full_html=False)
     
 
     # Render the index.html template with the plots
