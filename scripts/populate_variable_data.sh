62d45c7ac8629fdd0aaeea08ca078ed774d2382b
ipy06 <Ipinoluwaheze@gmail.com>
2024-08-13 22:24:47 -0500

    added script section for killing previous run instances of the current script

--- scripts/populate_variable_data.sh
+++ scripts/populate_variable_data.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-./collect_disk_space.sh &
+process_ids=$(ps -e | grep "populate" | awk '{print $1}' | sort -n) # extracts PIDs for the populate script
+
+if [ $(echo "$process_ids" | wc -l) -gt 2 ]; then # checks if the process_ids are greater than 1
+  oldest_process_id=$(echo "$process_ids" | head -n 1)
+  kill -9 $oldest_process_id &> /dev/null
+fi
+
+./collect_disk_space.sh
 for i in {1..60}; do
   ./collect_cpu_usage.sh &
   ./collect_memory_usage.sh &
